650833be12fac6b555544e5d4d8c5b6180432a1e
Andre <mctech26@gmail.com>
2018-03-17 21:07:03 +0100

    Styled contactUs.html and changed input to textarea for "Main text"

--- phpfiles/contactUs.php
+++ phpfiles/contactUs.php
@@ -1,22 +1,26 @@
 <html>
 <body>
-<?php
-/**
- * Created by PhpStorm.
- * User: seanp
- * Date: 15/03/2018
- * Time: 11:27
- */
+    <?php
+        /**
+         * Created by PhpStorm.
+         * User: seanp
+         * Date: 15/03/2018
+         * Time: 11:27
+         */
 
-$to = "synewaveltd@gmail.com";
-$headers = 'From: synewavecomplaints@gmail.com' . "\r\n" .
-    'Reply-To: ' . $_POST['email'] . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-$fullText = $_POST['fullName'] . "\r\n" . $_POST['mainText'] . "\r\n";
-mail($to, $_POST['subject'], $fullText, $headers);
+        $to = "synewaveltd@gmail.com";
 
-header("Location: http://127.0.0.1:8080/WebApplications/html-structures/frontPage.html"); /*redirects to main page*/
-exit();
-?>
+        $headers = 'From: synewavecomplaints@gmail.com' . "\r\n" .
+            'Reply-To: ' . $_POST['email'] . "\r\n" .
+            'X-Mailer: PHP/' . phpversion();
+
+        $fullText = "Complaint from : " . $_POST['email'] . "\r\n" . "Name : " .
+            $_POST['fullName'] . "\r\n" . $_POST['mainText'] . "\r\n";
+
+        mail($to, $_POST['subject'], $fullText, $headers);
+
+        header("Location: http://127.0.0.1:8080/WebApplications/html-structures/frontPage.html"); /*redirects to main page*/
+        exit();
+    ?>
 </body>
 </html>
