026712ebb97e0637f5b82a206e8d30f2aa7634f2
Russell <35470600+RussellSB@users.noreply.github.com>
2018-03-31 12:47:26 +0200

    Added "activemenu" to header

--- includes/header.php
+++ includes/header.php
@@ -5,10 +5,38 @@
 
     <!-- The left side of the header. Contains the title, and three menu items. -->
     <div id="leftside">
-        <a href="../page-structures/frontPage.php" class="navtext synewave">SyneWave</a>
-        <a href="../page-structures/products.php" class="navtext menus">Products</a>
-        <a href="../page-structures/aboutPage.php" class="navtext menus">About Us</a>
-        <a href="../page-structures/contactUs.php" class="navtext menus">Contact Us</a>
+        
+        <!--These are conditions that detect the active menu by getting the current php file name and setting which to highlight accordingly-->
+        <?php if(basename($_SERVER['PHP_SELF'])=="frontPage.php") :?>
+
+            <a href="../page-structures/frontPage.php" class="navtext synewave" id="activemenu">SyneWave</a>
+            <a href="../page-structures/products.php" class="navtext menus">Products</a>
+            <a href="../page-structures/aboutPage.php" class="navtext menus">About Us</a>
+            <a href="../page-structures/contactUs.php" class="navtext menus">Contact Us</a>
+
+        <?php elseif(basename($_SERVER['PHP_SELF'])=="products.php") : ?>
+
+            <a href="../page-structures/frontPage.php" class="navtext synewave">SyneWave</a>
+            <a href="../page-structures/products.php" class="navtext menus" id="activemenu">Products</a>
+            <a href="../page-structures/aboutPage.php" class="navtext menus">About Us</a>
+            <a href="../page-structures/contactUs.php" class="navtext menus">Contact Us</a>
+
+        <?php elseif(basename($_SERVER['PHP_SELF'])=="aboutPage.php") : ?>
+
+            <a href="../page-structures/frontPage.php" class="navtext synewave">SyneWave</a>
+            <a href="../page-structures/products.php" class="navtext menus">Products</a>
+            <a href="../page-structures/aboutPage.php" class="navtext menus" id="activemenu">About Us</a>
+            <a href="../page-structures/contactUs.php" class="navtext menus">Contact Us</a>
+
+        <?php elseif(basename($_SERVER['PHP_SELF'])=="contactUs.php") : ?>
+
+            <a href="../page-structures/frontPage.php" class="navtext synewave">SyneWave</a>
+            <a href="../page-structures/products.php" class="navtext menus">Products</a>
+            <a href="../page-structures/aboutPage.php" class="navtext menus">About Us</a>
+            <a href="../page-structures/contactUs.php" class="navtext menus" id="activemenu">Contact Us</a>
+
+        <?php endif; ?>
+
     </div>
 
     <!-- The right side of the header. Contains the search bar, button, and shopping button. -->
