666f6c711c4181d6186491725b6df5ed83e4f9bb
Sean Powell <seanpowell54@gmail.com>
2018-04-04 14:32:04 +0200

    Update contactUs.php
    Fixing conflict

--- phpfiles/contactUs.php
+++ phpfiles/contactUs.php
@@ -1,27 +1,26 @@
 <html>
 <body>
-    <?php
-        /**
-         * Created by PhpStorm.
-         * User: seanp & andrej & russellsb
-         * Date: 15/03/2018
-         * Time: 11:27
-         */
+<?php
+/**
+ * Created by PhpStorm.
+ * User: seanp
+ * Date: 15/03/2018
+ * Time: 11:27
+ */
 
-        $to = "synewaveltd@gmail.com";
+$to = "synewaveltd@gmail.com";
 
-        $headers = 'From: synewavecomplaints@gmail.com' . "\r\n" .
-            'Reply-To: ' . $_POST['email'] . "\r\n" .
-            'X-Mailer: PHP/' . phpversion();
+$headers = 'From: synewavecomplaints@gmail.com' . "\r\n" .
+    'Reply-To: ' . $_POST['email'] . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
 
-            $fullText = "Complaint from : " . $_POST['email'] . "\r\n" . "Name : " .
-                $_POST['fullName'] . "\r\n" . $_POST['mainText'] . "\r\n";
+$fullText = "Complaint from : " . $_POST['email'] . "\r\n" . "Name : " .
+    $_POST['fullName'] . "\r\n" . $_POST['mainText'] . "\r\n";
 
-            mail($to, $_POST['subject'], $fullText, $headers);
+mail($to, $_POST['subject'], $fullText, $headers);
 
-            header("Location: http://127.0.0.1:8080/WebApplications/page-structures/frontPage.php"); /*redirects to main page*/
-            exit();
-
-    ?>
+header("Location: http://127.0.0.1:8080/WebApplications/html-structures/frontPage.html"); /*redirects to main page*/
+exit();
+?>
 </body>
 </html>
