b29cc1632303997ff3c7ed686a96a3ba52d76eb4
MasterTextman <mctech26@gmail.com>
2018-04-29 22:29:42 +0200

    Added 404 page

---
+++ page-structures/404.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="description" content="How did you find this?">
+      <meta name="author" content="Syne Bros.">
+      <title>Nothin here ya goof</title>
+      <link rel="stylesheet" href="../stylesheets/3d.css"/>
+  </head>
+
+  <body>
+    <audio autoplay loop>
+      <source src="../includes/404music.mp3">
+  </audio>
+    <p class="spinner" text="404!">404!</p>
+    <p class="spinner" style="font-size: 60px;"
+        text="This page does not exist unfortunately! :(">
+        This page does not exist unfortunately! :(</p>
+    <p class="spinner" style="font-size: 30px;"
+        text="In the meantime, listen to this sweet tune! :)">
+        In the meantime, listen to this sweet tune! :)</p>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div align="center">
+      <a class="link" href="https://www.youtube.com/watch?v=yD2FSwTy2lw&feature=youtu.be">music source</a>
+    </div>
+  </body>
+
+</html>
