8aa9d85eaca5326e151e0fad8a6649558918d359
russellsb <r.sammutbonnici@gmail.com>
2018-04-29 22:01:14 +0200

    Added xeba ukuleles

--- settings/sqlData/MySQLData.sql
+++ settings/sqlData/MySQLData.sql
@@ -35,6 +35,7 @@ DROP TABLE IF EXISTS `productlist`;
 CREATE TABLE `productlist` (
   `ProductID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `Category` int(11) NOT NULL,
+  `Make` text COLLATE utf8_bin NOT NULL,
   `ProductName` text CHARACTER SET latin1 NOT NULL,
   `AmountInStock` int(11) NOT NULL,
   `Cost` decimal(5,2) NOT NULL,
@@ -43,7 +44,7 @@ CREATE TABLE `productlist` (
   PRIMARY KEY (`ProductID`),
   UNIQUE KEY `ProductID` (`ProductID`),
   KEY `ProductID_2` (`ProductID`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -52,7 +53,7 @@ CREATE TABLE `productlist` (
 
 LOCK TABLES `productlist` WRITE;
 /*!40000 ALTER TABLE `productlist` DISABLE KEYS */;
-INSERT INTO `productlist` VALUES (2,101,'Yamaha SA2200 Semi-Acoustic Electric',90,275.85,'','sa2200.png'),(3,102,'Yamaha GL-1 Ukulele',75,55.50,'Midget hipster guitar','gl1.jpg');
+INSERT INTO `productlist` VALUES (2,104,'Yamaha','Yamaha SA2200 Semi-Acoustic Electric',90,275.85,'The Yamaha SA2200 is a Japanese made semi-hollowbody electric guitar model that replicates Gibson\'s ES-335 classic save for the popular Far Eastern variation of thinner horns.','sa2200.jpg'),(3,103,'Yamaha','Yamaha GL-1 Guitalele',75,55.50,'The Yamaha GL1 Mini 6-String Nylon Guitalele is half guitar, half ukulele, and 100% fun. A unique mini 6-string nylon guitar that is sized like a baritone ukulele (17\" scale) and plays like a standard tuned guitar.','gl1.jpg'),(4,103,'Cordoba','Cordoba 15TM Tenor Ukulele',15,200.00,'A beautifully crafted Tenor Ukulele with an uplifting tone and cheerfully sturdy tune-heads.','15m.jpg'),(5,103,'Kala','Kala KA-15S Mahogany Soprano Ukulele',5,50.20,'A Soprano Ukulele with raw Hawaiian vibes. Buy now to play to your Ohana just like in Stitch.','ka15s.jpg'),(6,103,'Diamond Head','Diamond Head DU-150 Soprano Ukulele - Mahogany Brown',70,22.97,'Soprano ukulele with traditional shape. Produces full-bodied sound and packs a punch - if a punch consisted of rainbows, happiness and ever lasting peace.','du150.jpg'),(7,103,'Vorson','Vorson FSUK1BK Style Electric Ukulele',300,99.95,'Just when you thought ukuleles were lame. PA-POW PE-PEW, introducing the kick-ass Vorson FSUK1BK Style Electric Ukulele that kicks a kick into the kick with joyful tunes such as Highway to Hell!','fsuk1bk.jpg'),(8,103,'VIVICTORY','VIVICTORY Electric Ukulele Tenor VCE-T26 Aquila Nylon String',50,100.69,'This electric ukulele is made of solid mahogany. Not only does it look cute, but it plays cute too! Order now to impress your friends and family, as well as otakus who would find this extremely kawaii!','vcet26'),(9,103,'Epiphone','Epiphone Les Paul Acoustic/Electric Ukulele Outfit Heritage, Cherry Burst',60,122.50,'The legendary Epiphone Les Paul in Ukulele form. Rock out to Over the Rainbow with this classy rock look and feel.','eplpu.jpg'),(10,103,'Kala','Makala Kala Shark Soprano Ukulele',70,40.50,'This Soprano Ukulele has an Agathis top as well as a shark shaped bridge. Buy now and cheerfully jam to the Jaws theme song in Green style!','mksu.jpg'),(11,103,'Kala','Kala MK-SS/WHT Makala Shark Soprano Ukulele (White)',30,30.60,'This Soprano Ukulele has an Agathis top as well as a shark shaped bridge. Buy now and cheerfully jam to the Jaws theme song in White style!','mksuw.jpg'),(12,103,'Kala','Kala MK-SWT/Clear Makala Waterman Composite Soprano Ukulele in Clear Color',20,40.50,'Water resistant Ukulele. Super cool and easy to clean! Durable, high-grade poly-carbonate construction.','mkswt'),(13,103,'Kala','Kala Makala Ukadelic Camo Soprano Ukulele',70,47.70,'Has a distinctive art finish. Agathis wood top and injection molded body. The only drawback is that it\'s easy to lose. Keep an eye on this one - has too good of a sound to lose!','mkc.jpg'),(14,103,'Lanikai','Lanikai LK-SEU UkeSB Soprano Ukulele, Gloss Natural',300,220.50,'USB Output for Plug and Play recording. Genuine Mother-of-Pearl inlays. Buy now for high-class jamming to Over the Rainbow!','lkseu.jpg'),(15,103,'Luna','Luna Mahogany Series Honu Soprano Ukulele',150,76.73,'A TASTE OF THE TROPICS: Luna’s beautiful Honu Soprano Ukulele is from the exclusive Mahogany Series and is authentically designed to replicate traditional Hawaiian body ornamentation.','lmsh.jpg'),(16,103,'Luna','Luna Tattoo Mahogany Soprano Ukulele',142,99.99,'Soprano size. Rosewood fretboard. Great for playing Over the Rainbow - with a fancy look!','ltmsu.jpg'),(17,103,'TMS','TMS 21 Inch Soprano Ukulele Basswood Fingerboard & Bridge 12 Frets 4 String',720,15.95,'The TMS Maple body Soprano Ukulele offers a full-bodied tone with plenty of \"sweet highs\" and \"mellow lows\" that combine for a full rich sound. Chrome die-cast geared tuners assure your instrument will stay in tune.','tms21.jpg');
 /*!40000 ALTER TABLE `productlist` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -69,4 +70,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-04-23 15:07:19
+-- Dump completed on 2018-04-29 21:59:19
