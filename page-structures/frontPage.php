3f531a4f798246f842e77b33ad9f44e087ea264c
russellsb <r.sammutbonnici@gmail.com>
2018-05-01 10:51:56 +0200

    Added Keys Family

--- page-structures/frontPage.php
+++ page-structures/frontPage.php
@@ -16,7 +16,7 @@
 
 <header id="navbar">
     <nav>
-        <?php include "../includes/header.php"?>
+        <?php include "header.php"?>
     </nav>
 </header>
 
@@ -40,7 +40,7 @@
 </main>
 
 <footer>
-    <?php include "../includes/footer.php"?>
+    <?php include "footer.php"?>
 </footer>
 
 <!-- Added scripts necessary for site to function normally.
