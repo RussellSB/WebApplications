f67f307f93430041286f9797e230d51bca02b4da
MasterTextman <mctech26@gmail.com>
2018-05-15 22:50:02 +0200

    Syntax error fix

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -25,28 +25,25 @@ $subcategories = array
 
   <form action="<?= $currPath . "page-structures/products.php"?>" method="post">
 
-    <?php
-      // Loops through the main categories, displaying a button for each
-      for($i = 0; $i < sizeof($categories); $i++){
-        ?>
+    
+      <!-- Loops through the main categories, displaying a button for each -->
+      <?php for($i = 0; $i < sizeof($categories); $i++): ?>
         <button type="submit" name="query"
           value="<?= $QSTART . ($i+1) ?>"
           class="sidelinks">
           <p class="cat"> <?= $categories[$i] ?> </p>
         </button>
-        <?
 
-        // Loops through the subcategories, displaying another button for each
-        for($j = 0; $j < sizeof($subcategories[$i]); $j++){
-          ?>
+        <!-- Loops through the subcategories, displaying another button for each -->
+        <?php for($j = 0; $j < sizeof($subcategories[$i]); $j++): ?>
           <button type="submit" name="query"
           value="<?= $QSTART . ($i+1) . " and " . $QSUBC . ($j+1)?>"
           class="sidelinks subcat">
           <p class="subcat"> <?= $subcategories[$i][$j] ?> </p>
           </button>
-          <?
-        }
-      }
+          <?php
+        endfor;
+      endfor;
     ?>
 
       
