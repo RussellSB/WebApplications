0b2ac17fca4868607097b543a47efbab7ad1ec9b
russellsb <r.sammutbonnici@gmail.com>
2018-05-17 01:33:31 +0200

    reverted searching.php to normal

--- php-functions/searching.php
+++ php-functions/searching.php
@@ -25,36 +25,27 @@
 $input = $_GET["query"];
 debug_to_console($input);
 require "../php-functions/db.php";
-$rowsFound = "SELECT COUNT(*) FROM productlist WHERE UPPER(ProductName) like UPPER('%" . $input . "%');";
 $sql = "SELECT * FROM productlist WHERE UPPER(ProductName) like UPPER('%" . $input . "%');";
 $query = mysqli_query($con, $sql) or die(nl2br("\n Failed to execute query"));
 
-//if no rows are returned by the query
-if($rowsFound == 0){
-
-    alert("No results found!";
-
-}else{ //else, when returned
-
-    // Retrieves all the rows returned by the SQL query
-    $rows = array();
-    $x = 0;
-    echo "<div class=\"items\">";
-    while ($r = mysqli_fetch_assoc($query)) {
-        echo "<div class=\"product\">";
-        echo "<div class=\"image\">";
-        echo "<img src=http://$_SERVER[HTTP_HOST]/WebApplications/productImages/"
-            . $r['PictureLocation'] .
-            " alt=" . $r['PictureLocation'] . "></img>";
-        echo "</div>";
-        echo "<p> " . $r['ProductName'] . "</p>";
-        echo "<h3> $" . $r['Cost'] . "</h2>";
-        $x = $r['ProductID'];
-        echo "<button onClick='toProducts($x)' type='button' name='Product_Page' value=$x >" . "Product page" . "</button>";
-        echo "</div>";
+// Retrieves all the rows returned by the SQL query
+$rows = array();
+$x = 0;
+echo "<div class=\"items\">";
+while ($r = mysqli_fetch_assoc($query)) {
+    echo "<div class=\"product\">";
+    echo "<div class=\"image\">";
+    echo "<img src=http://$_SERVER[HTTP_HOST]/WebApplications/productImages/"
+        . $r['PictureLocation'] .
+        " alt=" . $r['PictureLocation'] . "></img>";
+    echo "</div>";
+    echo "<p> " . $r['ProductName'] . "</p>";
+    echo "<h3> $" . $r['Cost'] . "</h2>";
+    $x = $r['ProductID'];
+    echo "<button onClick='toProducts($x)' type='button' name='Product_Page' value=$x >" . "Product page" . "</button>";
+    echo "</div>";
 }
 
-}
 
 /* from https://stackoverflow.com/questions/4323411/how-can-i-write-to-console-in-php */
 function debug_to_console( $data ) {
