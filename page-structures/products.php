7078f7403efec67e40e4a8b8b5c7049e600c676b
russellsb <r.sammutbonnici@gmail.com>
2018-05-17 01:29:39 +0200

    Fixed bug for no search matches. TODO: stop redirection

--- page-structures/products.php
+++ page-structures/products.php
@@ -42,36 +42,49 @@
 
       $query = mysqli_query($con, $sql) or die(nl2br("\n Failed to execute query"));
 
-      if (mysqli_num_rows($query)==0) { header("location:404.php"); }
+      //if the query's results are 0
+      if (mysqli_num_rows($query) == 0) {
 
-      // Retrieves all the rows returned by the SQL query
-      $rows = array();
-      $x = 0;
-      echo "<div class=\"items\">";
-      while($r = mysqli_fetch_assoc($query)) {
+        $message = "llWe apologise but there were no matches found!";
+        echo "<script type='text/javascript'>alert('$message');</script>";
 
-          $maxPos = 100;
-          if (strlen($r['ProductName']) > $maxPos)
-          {
-              $lastPos = ($maxPos - 3) - strlen($r['ProductName']);
-              $r['ProductName'] = substr($r['ProductName'], 0, strrpos($r['ProductName'], ' ', $lastPos)) . '...';
-          }
-
-          echo "<div class=\"product\">";
-          echo "<div class=\"image\">";
-          echo "<img src=http://$_SERVER[HTTP_HOST]/WebApplications/productImages/"
-           . $r['PictureLocation'] .
-           " alt=" . $r['PictureLocation'] . "></img>";
-          echo "</div>";
-          echo "<p> " . $r['ProductName'] . "</p>";
-          echo "<h3> $" . $r['Cost']  . "</h2>";
-          $x = $r['ProductID'];
-          echo "<form action=\"singleproduct.php\" method=\"post\">";
-          echo "<button name=\"prod\" type=\"submit\" value=" . $r['ProductID'] . "> Buy Now </button>";
-          echo "</form>";
-        //   echo "<button onClick='toProducts($x)' type='button' name='Product_Page' value=$x >" . "Product page" . "</button>";
-          echo "</div>";
       }
+      
+      //else when the queries results are more than 0
+      else if (mysqli_num_rows($query) > 0){
+
+        // Retrieves all the rows returned by the SQL query
+        $rows = array();
+        $x = 0;
+        echo "<div class=\"items\">";
+        while($r = mysqli_fetch_assoc($query)) {
+
+            $maxPos = 100;
+            if (strlen($r['ProductName']) > $maxPos)
+            {
+                $lastPos = ($maxPos - 3) - strlen($r['ProductName']);
+                $r['ProductName'] = substr($r['ProductName'], 0, strrpos($r['ProductName'], ' ', $lastPos)) . '...';
+            }
+
+            echo "<div class=\"product\">";
+            echo "<div class=\"image\">";
+            echo "<img src=http://$_SERVER[HTTP_HOST]/WebApplications/productImages/"
+            . $r['PictureLocation'] .
+            " alt=" . $r['PictureLocation'] . "></img>";
+            echo "</div>";
+            echo "<p> " . $r['ProductName'] . "</p>";
+            echo "<h3> $" . $r['Cost']  . "</h2>";
+            $x = $r['ProductID'];
+            echo "<form action=\"singleproduct.php\" method=\"post\">";
+            echo "<button name=\"prod\" type=\"submit\" value=" . $r['ProductID'] . "> Buy Now </button>";
+            echo "</form>";
+            //echo "<button onClick='toProducts($x)' type='button' name='Product_Page' value=$x >" . "Product page" . "</button>";
+            echo "</div>";
+
+        }
+        
+      }
+      
     ?>
 
   </main>
