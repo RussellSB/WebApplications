55dec1622c1998b05f226230596b3542d7ebadff
russellsb <r.sammutbonnici@gmail.com>
2018-05-19 14:10:53 +0200

    Added more bot changes

--- php-functions/emailing.php
+++ php-functions/emailing.php
@@ -8,11 +8,11 @@
  * Time: 11:27
  */
 $to = "synewaveltd@gmail.com";
-$headers = 'From: synewavecomplaints@gmail.com' . "\r\n" .
+$headers = 'From: synewavebot@gmail.com' . "\r\n" .
     'Reply-To: ' . $_POST['email'] . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 $fullText = "Complaint from : " . $_POST['email'] . "\r\n" . "Name : " .
-    $_POST['fullName'] . "\r\n" . $_POST['mainText'] . "\r\n";
+    $_POST['fullName'] . "\r\n\n" . $_POST['mainText'] . "\r\n";
 mail($to, $_POST['subject'], $fullText, $headers);
 header("Location: http://127.0.0.1:8080/WebApplications/page-structures/frontPage.php"); /*redirects to main page*/
 exit();
--- php-functions/purchasing.php
+++ php-functions/purchasing.php
@@ -11,9 +11,10 @@ $to = "synewaveltd@gmail.com";
 $headers = 'From: synewavepurchases@gmail.com' . "\r\n" .
     'Reply-To: ' . $_POST['email'] . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
-$fullText = "Purchase from : " . $_POST['email'] . "\r\n" . "Name : " .
-    $_POST['fullName'] . "\r\n" . "Address : " .
-    $_POST['homeAddress'] . "\r\n";
+$fullText = "Purchase from : " . $_POST['email'] . 
+        "\r\n" . "Name : " .   $_POST['fullName'] . 
+        "\r\n" . "Address : " . $_POST['homeAddress'] . 
+        "\r\n";
 mail($to, "Purchase Order", $fullText, $headers);
 header("Location: http://127.0.0.1:8080/WebApplications/page-structures/frontPage.php"); /*redirects to main page*/
 exit();
