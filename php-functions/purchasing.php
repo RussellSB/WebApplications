df0ec8c88a4505f33bf9dfc9a05be241073a07cc
russellsb <r.sammutbonnici@gmail.com>
2018-05-19 14:52:17 +0200

    Setting up Purchase Order email information from session

--- php-functions/purchasing.php
+++ php-functions/purchasing.php
@@ -11,10 +11,22 @@ $to = "synewaveltd@gmail.com";
 $headers = 'From: synewavepurchases@gmail.com' . "\r\n" .
     'Reply-To: ' . $_POST['email'] . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
+
+$order;
+
+foreach($_SESSION as &$tobuy):
+        
+    $order .= "\r\n" . $tobuy['ProductID']  . $tobuy['Make'] . $tobuy['ProductName'] . $tobuy['Cost'] . $tobuy['quantity'];
+        
+endforeach
+    
 $fullText = "Purchase from : " . $_POST['email'] . 
-        "\r\n" . "Name : " .   $_POST['fullName'] . 
-        "\r\n" . "Address : " . $_POST['homeAddress'] . 
-        "\r\n";
+    "\r\n" . "Name : " .   $_POST['fullName'] . 
+    "\r\n" . "Address : " . $_POST['homeAddress'] . 
+    "\r\n\n" . "----------Products----------" .
+    "\r\n" . "ProductID:\tProductName\tQuantity\tCost(€)" . 
+    "\r\n" . $order . "\r\n";
+;
 mail($to, "Purchase Order", $fullText, $headers);
 header("Location: http://127.0.0.1:8080/WebApplications/page-structures/frontPage.php"); /*redirects to main page*/
 exit();
