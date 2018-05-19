c3fb1398f62c85a70be98574c1978966dbec61a5
russellsb <r.sammutbonnici@gmail.com>
2018-05-19 16:06:36 +0200

    Purchasing email succesfully works!

--- php-functions/purchasing.php
+++ php-functions/purchasing.php
@@ -2,25 +2,28 @@
 <body>
 <?php
 
+session_start();
+
 $to = "synewaveltd@gmail.com";
 $headers = 'From: synewavepurchases@gmail.com' . "\r\n" .
     'Reply-To: ' . $_POST['email'] . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
-$order;
+$order="";
 
-foreach($_SESSION as &$tobuy):
+//concatenates to order string variable
+foreach($_SESSION as &$tobuy){
         
-    $order .= $tobuy['ProductID']  . $tobuy['Make'] . wordwrap($tobuy['ProductName'], 30) . $tobuy['Cost'] . $tobuy['quantity'] . "\r\n";
+    $order .= $tobuy['ProductID']  . ", " . $tobuy['Make'] . ", " . $tobuy['ProductName'] . ", " . $tobuy['Cost'] . ", " . $tobuy['quantity'] . ";" . "\r\n";
         
-endforeach;
+}
     
 $fullText = "Purchase from : " . $_POST['email'] . 
     "\r\n" . "Name : " .   $_POST['fullName'] . 
     "\r\n" . "Address : " . $_POST['homeAddress'] . 
-    "\r\n\n" . "------------------------------------------------------------Products------------------------------------------------------------" .
-    "\r\n" . "ProductID:\t\tMake\t\tProductName\t\tQuantity\t\tCost(€)" . 
-    "\r\n" . $order . "\r\n";
+    "\r\n\n\n\n" . "------------------------------------------------------------Products------------------------------------------------------------" .
+    "\r\n" . "The Ordered Products are displayed in the order; ProductID, Make, ProductName, Cost(€), Quantity" . 
+    "\r\n\n" . $order . "\r\n";
 ;
 mail($to, "Purchase Order", $fullText, $headers);
 header("Location: http://127.0.0.1:8080/WebApplications/page-structures/frontPage.php"); /*redirects to main page*/
